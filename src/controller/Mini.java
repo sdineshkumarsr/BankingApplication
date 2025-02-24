9ab0e6efe68506e3f4f81e1be742ad1eaf4753c6
Zuzoo <sdineshkumarsr@gmail.com>
2025-02-25 00:26:49 +0530

    Project Completed

--- src/controller/Mini.java
+++ src/controller/Mini.java
@@ -17,23 +17,7 @@ public class Mini extends JFrame implements ActionListener {
         setLayout(null);
         setSize(400,600);
         setLocation(500,200);
-//        ImageIcon machine=new ImageIcon(ClassLoader.getSystemResource("images/atm.jpg"));
-//        Image img=machine.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
-//        ImageIcon img1=new ImageIcon(img);
-//        JLabel l1=new JLabel(img1);
-//        l1.setBounds(0,0,1150,830);
-//        add(l1);
-//        ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("images/logo.png"));
-//        Image i2=i1.getImage().getScaledInstance(50,50, Image.SCALE_DEFAULT);
-//        ImageIcon i3=new ImageIcon(i2);
-//        JLabel image=new JLabel(i3);
-//        image.setBounds(130,138,150,100);
-//        l1.add(image);
-//        JLabel label1=new JLabel("WELCOME TO MONEY BANK ATM");
-//        label1.setFont(new Font("Constantia",Font.BOLD,25));
-//        label1.setForeground(Color.white);
-//        label1.setBounds(340,180,450,35);
-//        l1.add(label1);
+
 
         JLabel label1= new JLabel("MINI STATEMENT");
         label1.setFont(new Font("Constantia",Font.BOLD,20));
